610e31eb9bc6fa6c33ad975f428079e32e5d4d97
MarcoGarthi97 <marcogarthi@gmail.com>
2025-05-07 19:55:23 +0200

    Aggiungere i file di progetto.

---
+++ PPDesk.Service/Collection/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PPDesk.Abstraction.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPDesk.Service.Collection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSharedLibrarySrvServices(this IServiceCollection services)
+        {
+            services.Scan(scan => scan
+            .FromAssemblies(Assembly.GetExecutingAssembly())
+            .AddClasses()
+            .AsImplementedInterfaces(typeInfo =>
+                typeInfo.GetInterfaces().Any(i =>
+                    i == typeof(IForServiceCollectionExtension) ||
+                    typeInfo == typeof(IForServiceCollectionExtension))
+            )
+            .WithScopedLifetime()
+            );
+
+            return services;
+        }
+    }
+}
