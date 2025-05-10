6aaaf2f439183f71508911e37e09b50099819714
MarcoGarthi97 <marcogarthi@gmail.com>
2025-05-10 21:12:02 +0200

    Service TicketClass Eventbride

---
+++ PPDesk.Service/Mapper/SrvTicketClassMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PPDesk.Abstraction.DTO.Response.Eventbride.TicketClass;
+using PPDesk.Abstraction.DTO.Service.Eventbrite;
+
+namespace PPDesk.Service.Mapper
+{
+    public class SrvTicketClassMapper : Profile
+    {
+        public SrvTicketClassMapper()
+        {
+            CreateMap<ETicketClassResponse, SrvETicketClass>();
+        }
+    }
+}
--- PPDesk.Service/Services/Window/SrvMainWindowService.cs
+++ PPDesk.Service/Services/Window/SrvMainWindowService.cs
@@ -20,13 +20,15 @@ namespace PPDesk.Service.Services.Window
         private readonly ISrvEOrganizationService _eOrganizationService;
         private readonly ISrvEEventService _eEventService;
         private readonly ISrvEOrderService _eOrderService;
+        private readonly ISrvETicketClassService _eTicketClassService;
 
-        public SrvMainWindowService(ISrvEOrganizationService eOrganizationService, ISrvEAuthenticationService eAuthenticationService, ISrvEEventService eEventService, ISrvEOrderService eOrderService)
+        public SrvMainWindowService(ISrvEOrganizationService eOrganizationService, ISrvEAuthenticationService eAuthenticationService, ISrvEEventService eEventService, ISrvEOrderService eOrderService, ISrvETicketClassService eTicketClassService)
         {
             _eOrganizationService = eOrganizationService;
             _eAuthenticationService = eAuthenticationService;
             _eEventService = eEventService;
             _eOrderService = eOrderService;
+            _eTicketClassService = eTicketClassService;
         }
 
         public async Task Test()
@@ -35,6 +37,7 @@ namespace PPDesk.Service.Services.Window
             await _eOrganizationService.LoadOrganizationsAsync();
             var events = await _eEventService.GetListEventsByOrganizationIdAsync();
             var orders = await _eOrderService.GetListOrdersByOrganizationIdAsync();
+            var ticketClasses = await _eTicketClassService.GetListTicketClassesByEventIdsAsync(events.Select(x => x.Id));
         }
     }
 }
