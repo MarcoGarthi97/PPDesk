8d785c755aeafdf0daa0ed954a2903e6516f3695
MarcoGarthi97 <marcogarthi@gmail.com>
2025-06-06 00:54:44 +0200

    wip

---
+++ PPDesk.Service/Services/PP/SrvBackgroundServiceOrchestratorService.cs
@@ -0,0 +1,19 @@
+﻿using PPDesk.Abstraction.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPDesk.Service.Services.PP
+{
+    public interface ISrvBackgroundServiceOrchestratorService : IForServiceCollectionExtension
+    {
+
+    }
+
+    public class SrvBackgroundServiceOrchestratorService : ISrvBackgroundServiceOrchestratorService
+    {
+
+    }
+}
