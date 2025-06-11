105d86eb3048bac42167f834394258416c133b91
MarcoGarthi97 <marcogarthi@gmail.com>
2025-06-12 00:02:23 +0200

    Path database

--- PPDesk.Abstraction/DTO/Service/PP/SrvDatabaseConfiguration.cs
+++ PPDesk.Abstraction/DTO/Service/PP/SrvDatabaseConfiguration.cs
@@ -8,6 +8,7 @@ namespace PPDesk.Abstraction.DTO.Service.PP
 {
     public class SrvDatabaseConfiguration
     {
+        public string Path { get; set; }
         public bool DatabaseExists { get; set; }
         public bool LoadFast {  get; set; }
     }
